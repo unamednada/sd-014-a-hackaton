2d19d7a8a6e6b537bdb0e8f01691c33154fc5af2
Flavio Pires <flavioasp77@gmail.com>
2021-08-24 17:39:03 -0300

    funçao fecthQuiz() implementada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-// Make the function fetch
+const fetch = require('node-fetch');
+// Função que faz a requisição para a API 
+// category, dificult, limit
+const fetchQuiz = async (category, difficulty, limit) => {
+  // faz a reuisição da API com os parametros
+  const getQuestions = await fetch(`https://quizapi.io/api/v1/questions?apiKey=Sv36yp6PpNiHdkVuzwgj6XSswZTUPlrKoh7P6KZH&category=${category}difficulty=${difficulty}&limit=${limit}`);
+  // trata os dados convertendo em ObjectJson
+  const responseQuestions = await getQuestions.json();
+};
+
+console.log(fetchQuiz());
