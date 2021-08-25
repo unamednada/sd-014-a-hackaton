6a506920e979bc93c44f991b9290de5e7fe69016
unamednada <dias.gustavo.dias@gmail.com>
2021-08-24 21:08:15 -0300

    Add valuable comments and create createquiz, responsible for integrating all fetch-related functions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 // Make the function fetch 
 
 // const fetch = require('node-fetch');
+
 // Função que faz a requisição para a API 
 const fetchQuiz = async (params) => {
   const url = 'https://quizapi.io/api/v1/questions?apiKey=Sv36yp6PpNiHdkVuzwgj6XSswZTUPlrKoh7P6KZH';
   let endpoint = url;
+  // Verifica se há parametros a serem acrescentados
   if (params.category) endpoint += `&category=${params.category}`;
   if (params.difficulty) endpoint += `&difficulty=${params.difficulty}`;
   if (params.limit) endpoint += `&limit=${params.limit}`;
-  // faz a reuisição da API com os parametros
+  // faz a reuisição da API
   const getQuestions = await fetch(endpoint);
-  // const getQuestions = await fetch(`https://quizapi.io/api/v1/questions?apiKey=Sv36yp6PpNiHdkVuzwgj6XSswZTUPlrKoh7P6KZH&category=${category}&difficulty=${difficulty}&limit=${limit}`);
   // trata os dados convertendo em ObjectJson
   const responseQuestions = await getQuestions.json();
   return responseQuestions;
 };
-
-
 
 // Create createQuestionItem
 
@@ -28,6 +27,7 @@ const createQuestionItem = ({ question, answers, correct_answers }) => {
   questionDiv.innerHTML = `<h2 class="question">${question}</h2>`;
   // Itera por todas as respostas válidas e verifica se é a correta para então acrescentar o elemeto à questionDiv
   validAnswers.forEach((answer, index) => {
+    // Problemas quando as respostas são tags HTML - Modificar forma de acrescentar li à questionDiv !!!
     if (Object.values(correct_answers)[index] === 'true') questionDiv.innerHTML += `<li class="correct answer">${answer}</li>`;
     else questionDiv.innerHTML += `<li class="answer">${answer}</li>`
   });
@@ -38,18 +38,28 @@ const createQuestionItem = ({ question, answers, correct_answers }) => {
   return questionDiv;
 }
 
+// Create appendQuestions
+
 const appendQuestions = (container, questions) => {
   questions.forEach((questionObj) => {
     container.appendChild(createQuestionItem(questionObj));
   })
 }
 
-window.onload = async () => {
+// Create integration function
+
+const CreateQuiz = async () => {
+  //Aqui vamos pegar os valores dos parametros, se houver e chamar as funções necessárias para criar o quiz
   const params = {
     category: 'sql',
     limit: '10'
   }
+
   const questions = await fetchQuiz(params);
   appendQuestions(document.querySelector('#question-container'), questions);
+}
+
+window.onload = async () => {
+  await CreateQuiz();
 }
 
