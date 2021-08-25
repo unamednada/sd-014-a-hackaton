f893e6a532f2390b2908b1253f9df95ebc5479e3
unamednada <dias.gustavo.dias@gmail.com>
2021-08-24 23:14:59 -0300

    Create const mockreturn for test

--- test/script.test.js
+++ test/script.test.js
@@ -4,48 +4,31 @@ const script = require('../js/script.js');
 
 describe ('Testa se a função fetchQuiz', () => {
 
+  const mockReturn = [{
+    question: 'What is the correct way to add 1 to the $count variable?',
+    answers: {
+      a: '++count',
+      b: 'count++',
+      c: '$count =+1',
+      d: '$count++;',
+      e: null,
+      f: null
+    },
+    correct_answers: {
+      a:'false',
+      b: 'false',
+      c: 'false',
+      d: 'true',
+      e: 'false',
+      f: 'false'
+    }
+  }];
+
   fetch.mockImplementation(async (endpoint) => ({
-    json: async () =>
-    [{
-      question: 'What is the correct way to add 1 to the $count variable?',
-      answers: {
-        a: '++count',
-        b: 'count++',
-        c: '$count =+1',
-        d: '$count++;',
-        e: null,
-        f: null
-      },
-      correct_answers: {
-        a:'false',
-        b: 'false',
-        c: 'false',
-        d: 'true',
-        e: 'false',
-        f: 'false'
-      }
-    }]
+    json: async () => mockReturn
   }));
 
   it('retorna um array ao ser chamada sem parâmetros', async () => {
-    await expect(script.fetchQuiz()).resolves.toEqual([{
-      question: 'What is the correct way to add 1 to the $count variable?',
-      answers: {
-        a: '++count',
-        b: 'count++',
-        c: '$count =+1',
-        d: '$count++;',
-        e: null,
-        f: null
-      },
-      correct_answers: {
-        a:'false',
-        b: 'false',
-        c: 'false',
-        d: 'true',
-        e: 'false',
-        f: 'false'
-      }
-    }]);
+    await expect(script.fetchQuiz()).resolves.toEqual(mockReturn);
   })
-})
+});
